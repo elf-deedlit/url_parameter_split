3a40e7c42cf1bf1387efa290ab6dae6a7ef3ab7e
DENCHU <spam@denchu.jp>
2023-07-16 11:40:28 +0900

    実行権限付与、改行コード変更

--- url_parameter_split.py
+++ url_parameter_split.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # vim: set ts=4 sw=4 et smartindent ignorecase fileencoding=utf8:
 import argparse
 from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
